a3ce94ef4616c052c887f5be569b4de3b2fdaf97
Amit Jha <amitnjha@gmail.com>
2025-02-08 09:51:23 -0600

    2.8.2025 office

---
+++ cprog/temp_conv2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(){
+
+  int lower, upper, step;
+  float fahr, cel;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+
+  while(fahr <= upper){
+
+    cel = (5.0/9.0) * (fahr - 32);
+    printf("%3.1f <--> %6.1f\n", fahr , cel);
+    fahr += step;
+    
+  }
+}
